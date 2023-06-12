fc74aa25815afdaa352d341254c89a82b5082d6b
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-12 09:21:20 +0100

    feat: adds logging

--- src/handlers/functions/get-tenant-a-record.ts
+++ src/handlers/functions/get-tenant-a-record.ts
@@ -1,5 +1,4 @@
 import {ListResourceRecordSetsCommand, Route53Client} from '@aws-sdk/client-route-53'
-import {ResourceRecordSet} from '@aws-sdk/client-route-53/dist-types/models/models_0'
 
 interface GetTenantARecordProps {
   hostedZoneId: string
@@ -16,11 +15,10 @@ export const getTenantARecord = async (props: GetTenantARecordProps) => {
 
   const command = new ListResourceRecordSetsCommand(params)
   const response = await route53Client.send(command)
+  console.log('LISTED ITEMS: ', response)
 
   if (response.ResourceRecordSets) {
-    return response.ResourceRecordSets.find((item: ResourceRecordSet) =>
-      item.Name?.includes(tenantName),
-    )
+    return response.ResourceRecordSets.find((item) => item.Name?.includes(tenantName))
   }
 
   return null
